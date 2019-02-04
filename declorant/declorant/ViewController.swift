442dc86d30df526edda2704f99586b23e0947b72
yadik64 <yadik64@gmail.com>
2019-02-05 02:20:16 +0400

    I created a project

---
+++ declorant/declorant/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  declorant
+//
+//  Created by Дмитрий Яровой on 05/02/2019.
+//  Copyright © 2019 Дмитрий Яровой. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
